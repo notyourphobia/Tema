3384928d449e5e15487431a37cc9b3a93e002206
notyourphobia <notyourphobia@gmail.com>
2020-01-20 19:15:43 +0200

    Updates

---
+++ precache-manifest.f59c79c854496e5d18ddbf2dfae582c7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e6700855141f7c2e78a592b602785f",
+    "url": "/Tema/index.html"
+  },
+  {
+    "revision": "59bc0e818fffd1832a16",
+    "url": "/Tema/static/css/main.da87e27c.chunk.css"
+  },
+  {
+    "revision": "fdc3086fb1785efbfddb",
+    "url": "/Tema/static/js/2.899f8e26.chunk.js"
+  },
+  {
+    "revision": "1a3ba1185bf697297fa4b180bfc93f40",
+    "url": "/Tema/static/js/2.899f8e26.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59bc0e818fffd1832a16",
+    "url": "/Tema/static/js/main.bd37c425.chunk.js"
+  },
+  {
+    "revision": "58446c5536c3dc411ccd",
+    "url": "/Tema/static/js/runtime-main.b288c421.js"
+  },
+  {
+    "revision": "c2d4d07888d5dd0c8b98c72f3360e3fe",
+    "url": "/Tema/static/media/houses.c2d4d078.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tema/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b8f8f1a920ae18f074fa6bc1c7bea4b1",
+    "url": "/Tema/static/media/video.b8f8f1a9.png"
+  }
+]);
